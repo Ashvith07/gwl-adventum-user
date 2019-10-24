8f3b2a184ff5fe32638ae1479f2c47c3b0efe647
adventum-dev <rijo.thomas@adventum.ch>
2019-10-24 17:34:37 +0530

    first commit

---
+++ semantic-segmentation-editor-master_seperate/imports/common/SseBranding.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class SseSnackbar extends React.Component {
+    render() {
+        return (<div className="sse-logo hflex flex-align-items-center flex-justify-content-start">
+            <div className="vflex">
+            <img src="/ophtalma logo.png"/>
+                {/* <span>Semantic</span>
+                <span>Segmentation</span>
+                <span>Editor</span> */}
+               
+
+            </div>
+        </div>)
+    }
+}
---
+++ semantic-segmentation-editor-master_seperate/imports/navigator/SseAllUpload.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+export default class SseAllUpload extends React.Component {
+
+    constructor() {
+        super();
+       
+        this.state = {};
+
+        
+
+    };
+
+    componentDidMount() {
+     
+    }
+
+    componentWillUnmount(){
+    }
+
+    
+
+    render() {
+        return (
+            <div>
+                <h1>Layers</h1>
+              
+            </div>
+        );
+    }
+}
